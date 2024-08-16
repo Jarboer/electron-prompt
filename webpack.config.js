a9e0b9f28a379b12fb869810c3af4788b2a92d98
Jarrett <jar.boersen@gmail.com>
2024-08-16 14:44:27 -0400

    Made it so webpack just copies files for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,107 +1,60 @@
-// TODO: Make automatic (mainly the web stuff using loops)
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = [
-  // Node
+  // Node Configuration
   {
     watch: false,
-    mode: 'development', //   mode: 'production',
-    devtool: 'source-map', // TODO: Remove on publish
+    mode: 'development', // TODO: Switch to 'production' later
+    // TODO: Remove on publish
+    // devtool: 'source-map', // Use inline-source-map for better debugging during development
     target: 'node', // use require() & use NodeJs CommonJS style
     externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
     externalsPresets: {
       node: true, // in order to ignore built-in modules like path, fs, etc.
     },
     entry: {
-      index: { import: './src/index.js', filename: './[name].js' }, // filename: './[name].bundle.js'
+    //   index: { import: './src/index.js', filename: './[name].js' },
+    //   prompt: { import: './src/page/prompt.js', filename: './page/[name].js' },
     },
     output: {
-      path: path.resolve(__dirname, 'lib'), // Set output directory to 'lib'
-      pathinfo: true, // For debugging
+      path: path.resolve(__dirname, 'lib'),
+      // pathinfo: true, // For debugging
     },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    module: {
-      rules: [
-        // {
-        //   test: /\.ts$/,
-        //   use: 'ts-loader',
-        //   exclude: /node_modules/,
-        // },
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: ['index.js'], // Only clean the main section
-      }),
-    ],
-  },
-  // Web
-  {
-    watch: false,
-    mode: 'development', //   mode: 'production',
-    devtool: 'inline-source-map', // TODO: Remove on publish
-    target: 'web', // use require() & use NodeJs CommonJS style
-    // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-    // externalsPresets: {
-    //   node: true, // in order to ignore built-in modules like path, fs, etc.
+    // resolve: {
+    //   extensions: ['.ts', '.js'],
     // },
-    entry: {
-      prompt: {
-        import: './src/page/prompt.js',
-        filename: 'page/[name].js', // 'lib/page/[name].bundle.js'
-      },
-    },
-    output: {
-      path: path.resolve(__dirname, 'lib'), // Set output directory to 'lib'
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    module: {
-      rules: [
-        // {
-        //   test: /\.ts$/,
-        //   use: 'ts-loader',
-        //   exclude: /node_modules/,
-        // },
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   type: 'asset/resource',
-        // },
-        //   {
-        //     test: /\.css$/,
-        //     use: ['style-loader', 'css-loader'],
-        //   },
-        //   {
-        //     test: /\.html$/,
-        //     use: 'html-loader',
-        //   },
-      ],
-    },
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.ts$/,
+    //       use: 'ts-loader',
+    //       exclude: /node_modules/,
+    //     },
+    //   ],
+    // },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: 'src/page/prompt.html',
-        chunks: ['prompt'],
-        filename: 'page/prompt.html',
-        inject: false, // Disable automatic injection
-      }),
+      // new HtmlWebpackPlugin({
+      //   template: 'src/page/prompt.html',
+      //   chunks: ['prompt'],
+      //   filename: './page/prompt.html',
+      //   inject: false, // Disable automatic injection
+      // }),
+      // new CopyWebpackPlugin({
+      //   patterns: [
+      //     { from: 'src/page/prompt-out.css', to: './page' },
+      //   ],
+      // }),
       new CopyWebpackPlugin({
         patterns: [
-          {
-            from: 'src/page/prompt-out.css',
-            to: 'page',
-          }
+          { from: './**/*', to: '[path][name][ext]', context: 'src' }, // Copy all files
         ],
       }),
       new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: ['page/*'], // Only clean the main section
+        cleanOnceBeforeBuildPatterns: ['*'], // Clean the output before building
       }),
     ],
   },
